e370fe770b16c19781b592fcdb07e4046ca3e0b6
Ahmad-Ali-Al-Irsyadi <ahmadaliirsyad12@gmail.com>
2026-01-12 20:09:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 1. Logika untuk Glow di Tombol (Social Items)
+
 document.querySelectorAll('.social-item').forEach(button => {
     button.addEventListener('mousemove', e => {
         const rect = button.getBoundingClientRect();
@@ -9,16 +9,14 @@ document.querySelectorAll('.social-item').forEach(button => {
     });
 });
 
-// 2. Logika BARU untuk Glow di Balik Kartu (Card Glow Line)
 const card = document.querySelector('.card');
 card.addEventListener('mousemove', e => {
     const rect = card.getBoundingClientRect();
     
-    // Hitung posisi kursor relatif terhadap card
+
     const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
-    // Set variable CSS untuk card glow
     card.style.setProperty('--card-x', `${x}px`);
     card.style.setProperty('--card-y', `${y}px`);
 });
